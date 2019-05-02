b1811afc58090300d8677c740a43722468206e34
Tanzim Azad Nishan <nishan.tan.2015@gmail.com>
2019-05-02 21:18:48 +0600

    first version of C code

---
+++ string/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char str[]="01212";
+    char str1[]="012";
+    printf("%d",strspn(str,str1));
+    //printf("Hello world!\n");
+    return 0;
+}
